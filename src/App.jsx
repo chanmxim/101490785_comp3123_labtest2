69433269796f21ed6f03dac3211a59fa3161d967
chanmxim <chanmxim@gmail.com>
2025-11-27 18:47:06 -0500

    Complete app

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,62 @@
 import { useEffect, useState } from "react"
-import { getWeatherByCity } from "./api/weatherAPI";
+import { getDailyForecastByCity, getWeatherByCity } from "./api/weatherAPI";
+import { CityWeatherCard } from "./components/CityWeatherCard"
 
 function App() {
   const [city, setCity] = useState("Toronto");
+  const [inputCity, setInputCity] = useState('Toronto');
   const [weatherData, setWeatherData] = useState(null);
+  const [forecastList, setForecastList] = useState([])
   const [error, setError] = useState("");
 
   useEffect(() => {
     const getWeather = async () => {
       try{
-        const response = await getWeatherByCity(city);
-        setWeatherData(response);
+        const responseWeatherData = await getWeatherByCity(city);
+        setWeatherData(responseWeatherData.data);
+        
+        const { lat, lon } = responseWeatherData.data.coord;
+
+        const responseForecastList = await getDailyForecastByCity(lat, lon);
+
+        setForecastList(responseForecastList.data.list.slice(0, 4));
+
         setError("")
+
       } catch (e) {
         setError("City Not Found");
+        console.error("Error: ", e)
         setWeatherData(null);
+        setForecastList([])
       }
     }
 
     getWeather()
-  }, []);
+  }, [city]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCity(inputCity);
+  };
 
   return (
-    <div>
-      <h1>Weather App</h1>
+    <div className="container py-4">
+      <h1 className="text-center mb-4">Weather App</h1>
+
+      <form onSubmit={handleSubmit} className="d-flex justify-content-center mb-3 input-group">
+        <input
+          type="text"
+          className="form-control"
+          value={inputCity}
+          onChange={(e) => setInputCity(e.target.value)}
+          placeholder="Enter city..."
+        />
+        <button type="submit" className="btn btn-primary">Search</button>
+      </form>
+
+      {error && <p className="text-danger text-center fw-semibold">{error}</p>}
+
+      <CityWeatherCard weatherData={weatherData} forecastList={forecastList}/>
     </div>
   )
 }
--- src/components/CityWeatherCard.jsx
+++ src/components/CityWeatherCard.jsx
@@ -1,34 +1,66 @@
-export default function CityWeatherCard (data){
-    if (!data) return null;
+import { CityWeatherStat } from "./CityWeatherStat"
+import { ForecastItem } from "./ForecastItem";
 
-    const { name, main, weather, wind, visibility, clouds, sys } = data; 
+export function CityWeatherCard ({weatherData, forecastList}){
+    if (!weatherData) return null;
+
+    const { name, main, weather, wind, visibility, clouds, sys } = weatherData; 
 
     return (
-        <div className="card text-center">
-            <div className="card-body">
-                <h3 className="card-title">{name}, {sys.country}</h3>
+        <div className="card shadow-lg rounded-4 weather-card mt-5 mx-auto" id="wrapper">
+            <div className="card-body p-4 p-lg-5 row g-4">
+                <div className="col-12 col-lg-4 d-flex flex-column align-items-center justify-content-center p-4 main-weather-display rounded-3 shadow-sm">
+                    <div className="blue-background">
+                        <h3 className="fs-2 fw-bold text-black mb-1">{name}, {sys.country}</h3>
 
-                <div className="temperature">
-                    <img 
-                        src="{`http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`}" 
-                        alt="Weather Icon" 
-                    />
-                    <h4 className="card-text">Temp: {Math.round(main.temp - 273.15)}&deg;C</h4>
-                </div>  
+                        <div className="s-5 text-muted mb-4 text-capitalize d-flex justify-content-center align-items-center">
+                            <h4 className="fs-1 fw-bolder text-black">{Math.round(main.temp - 273.15)}&deg;C</h4>
+                        </div>  
+                    </div>
 
-                <div className="description">  
-                    <p className="card-text">Temp: {Math.round(main.feels_like - 273.15)}&deg;C. {weather.description}</p>
+                </div>
 
-                    <div className="weatherDetails">
-                        <p className="">Wind: {wind.speed} m/s</p>
-                        <p className="">Humidity: {main.humidity} %</p>
-                        <p className="">Pressure: {main.pressure} hPa</p>
-                        <p className="">Sea Level: {main.sea_level} hPa</p>
-                        <p className="">Ground Level: {main.grnd_level} hPa</p>
-                        <p className="">Cloudiness: {clouds.all} %</p>
-                        <p className="">Visibility: {visibility} m</p>
+                <div className="col-12 col-lg-8">
+                    <div className="row g-4">
+
+                        {/* Weather Statistics */}
+                        <div className="col-12 col-md-6">
+                            <div className="card h-100 shadow-sm border-0">
+                                <div className="card-body p-3 ">
+                                    <h4 className="fs-5 fw-semibold text-secondary mb-3 pb-2 border-bottom">Current Conditions</h4>
+                                    <div className="d-flex flex-column">
+                                        <CityWeatherStat key={0} label="Humidity" value={main.humidity} unit="%"/>
+                                        <CityWeatherStat key={1} label="Pressure" value={main.pressure} unit="hPa"/>
+                                        <CityWeatherStat key={2} label="Wind" value={wind.speed} unit="m/s"/>
+                                        <CityWeatherStat key={3} label="Visibility" value={visibility} unit="%"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Weather Forecast */}
+                        <div className="col-12 col-md-6">
+                            <div className="card h-100 shadow-sm border-0">
+                                <div className="card-body p-3 d-flex flex-column">
+                                    <h4 className="fs-5 fw-semibold text-secondary mb-3 pb-2 border-bottom">
+                                        5-Day Forecast
+                                    </h4>
+                                    <div >
+                                        {forecastList?.map((forecastItem, index) => (
+                                        < ForecastItem 
+                                            key={index}
+                                            date={forecastItem.dt} 
+                                            iconURL={`http://openweathermap.org/img/wn/${forecastItem.weather[0].icon}@2x.png` } 
+                                            temp={forecastItem.main.temp}/>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                
                     </div>
                 </div>
+                
             </div>
         </div>
     )
